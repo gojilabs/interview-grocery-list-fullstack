70ecd7a5351938f4442563ec465ff0ad77eb5569
Eugene Monakov <eugene@gojilabs.com>
2024-06-02 22:28:06 +0300

    Scaffolded backend api server

---
+++ api/prisma/migrations/20240602184929_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+INSERT INTO "User" ("email", "password", "createdAt", "updatedAt") VALUES ('testuser@example.com', 'testpassword', now(), now());
